84c3dd2a8a8e2f6acb676838ccc76459453ccb1d
ctw20070120 <2704406088@qq.com>
2025-10-20 22:06:19 +0800

    Update 2_main.c

--- 2_main.c
+++ 2_main.c
@@ -1,1 +1,11 @@
-
+//202510111332
+//2704406088@qq.com
+//陈亭伍
+#include <stdio.h>
+int main(){
+    float celsius,fahrenheit;
+    scanf("%f",&celsius);
+    fahrenheit=celsius*9/5+32;
+    printf("%.1f\n",fahrenheit);
+    return 0
+}
