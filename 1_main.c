3eb44b34cd27ace7c32c7a2bc548a14744ce9599
ctw20070120 <2704406088@qq.com>
2025-10-20 21:59:53 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,23 @@
-//
-//
-//
+//202510111332
+//2704406088@qq.com
+//陈亭伍
 #include <stdio.h>
-int main()
-{
-    printf("Hello World");
+int main(){
+    int score;
+    scanf("%d",&score);
+    if (score>=90 && score<=100){
+        printf("A\n");
+    }else if(score>=80 && score<=89){
+        printf("B\n");
+    }else if(score>=70 && score<=79){
+        printf("C\n");
+    }else if(score>=60 && score<=69){
+        printf("D\n");
+    }else if(score>=0 && score<=69){
+        printf("E\n");
+    }else{
+        printf("成绩无效");
+    }
     return 0;
-}
+
+    }
