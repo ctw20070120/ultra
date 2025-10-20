415bc2f30f21d86104c2c9c85bdbca1dddfe1f45
ctw20070120 <2704406088@qq.com>
2025-10-20 22:09:03 +0800

    Update 4_main.c

--- 4_main.c
+++ 4_main.c
@@ -1,1 +1,15 @@
+//202510111332
+//2704406088@qq.com
+//陈亭伍
+#include <stdio.h>
 
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    if (a + b > c && a + c > b && b + c > a) {
+        printf("可以组成三角形\n");
+    } else {
+        printf("不能组成三角形\n");
+    }
+    return 0;
+}
